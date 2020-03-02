51cf856a255e57efcb27c785198b65a581ac8daa
Perttu Paloniemi <Perttu.paloniemi@edu.turkuamk.fi>
2020-03-02 14:30:18 +0200

    Create Chapter2_notes.cs

---
+++ Chapter2_notes.cs
@@ -0,0 +1,17 @@
+Ohjausmerkki            Kuvaus
+\'                      Heittomerkki(Single Quote)
+\""                     Lainausmerkki(Double Quote)
+\\                      Kenoviiva(Backlash)
+\0                      Null-merkki(huom.: ei ole sama kuin C# null arvo)
+\a                      Varoitus(alert)
+\b                      Askelpalautus(Backspace)
+\f                      Sivun kelaus(Form feed)
+\n                      Uusi rivi(New line)
+\r                      Vaunun palautus(Carriage return)
+\t                      Sarkain(Horizontal tab)
+\v                      Pystysarkain(Vertical tab)
+
+Seuraavassa chart-tyyppinen muuttuja alustetaan erikoismerkeillä ja heksadesimaaliluvulla;
+
+01 charpSarkain='\v';
+02 char heksaLuku='\x0af';
